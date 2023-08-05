2b0e27a36b1c7486ac8cdc0e5f752c4736781700
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-08-05 14:36:30 -0300

    feat: alterações

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -3,7 +3,7 @@ import { FileArrowDown } from "phosphor-react";
 import { Button } from "../../components/Button";
 import { AboutInfo } from "./Info";
 
-//import cv from "../../assets/denismoreira.pdf";
+// import CV from "../../assets/cv-denis-moreira.pdf";
 
 import { StyledAbout } from "./styles";
 
@@ -18,23 +18,26 @@ export function About() {
                <AboutInfo />
 
                <p>
-                  Eu sou Denis Moreira, profissional graduando em Análise e
-                  Desenvolvimento de Sistemas, especializado em desenvolvimento
-                  front-end e aplicações voltadas para a interface do usuário.
-                  <br />
-                  Os avanços tecnológicos vêm carregados de facilidades que
-                  objetivam melhorar nosso desempenho nas tarefas do dia a dia,
-                  seja tornando-as mais rápidas, mais seguras ou mais simples.
-                  Fascinado pelos avanços tecnológicos, estou sempre em busca de
-                  novos desafios para poder progredir na área. Meu objetivo é
-                  encontrar as melhores soluções criativas, cuidando sempre de
-                  cada detalhe e buscando sempre um impacto positivo na
-                  experiencia do usuário. Me considero ágil e consistente em
-                  aprender uma nova tecnologia e alta adaptabilidade às
-                  mudanças, pronto para superar qualquer desafio! Possuo foco em
-                  desenvolvimento com React, React Native, Nextjs, HTML, CSS,
-                  Javascript, TypeScript e NodeJS dentre outras tecnologias que
-                  uso para criar aplicações web de alto nível.
+                  Eu sou Denis Moreira, um profissional em crescimento no campo
+                  de Análise e Desenvolvimento de Sistemas, com uma
+                  especialização em desenvolvimento front-end e aplicações
+                  centradas na experiência do usuário. Compreendo que os avanços
+                  tecnológicos trazem consigo soluções que visam aprimorar nossa
+                  eficiência diária, seja tornando tarefas mais ágeis, seguras
+                  ou simples. Essa fascinação pelos avanços tecnológicos me
+                  impulsiona a buscar constantemente novos desafios para
+                  progredir na minha área. Meu foco é encontrar soluções
+                  criativas que considerem minuciosamente cada detalhe, com o
+                  objetivo de proporcionar uma experiência do usuário
+                  excepcional e impactante. Minha capacidade de aprender
+                  rapidamente novas tecnologias e minha habilidade em me adaptar
+                  a mudanças me tornam ágil e consistente. Estou sempre pronto
+                  para enfrentar qualquer desafio que se apresente. Tenho foco
+                  em desenvolvimento utilizando React, React Native, Next.js,
+                  HTML, CSS, JavaScript, TypeScript e Node.js, entre outras
+                  tecnologias. Utilizo essas ferramentas para criar aplicações
+                  web de alta qualidade, sempre visando atingir os mais altos
+                  padrões.
                </p>
                <div className="div_button">
                   <Button icon={<FileArrowDown />}>
