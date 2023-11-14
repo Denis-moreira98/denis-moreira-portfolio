6f606c991f9ae2d42cc44f18586dbef12f68f4be
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-11-14 13:23:12 -0300

    feat: Priorizando darkmod

--- src/hooks/useDarkMode.tsx
+++ src/hooks/useDarkMode.tsx
@@ -1,50 +1,53 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import {
+   createContext,
+   ReactNode,
+   useContext,
+   useEffect,
+   useState,
+} from "react";
 
 interface DarkModeProviderProps {
-  children: ReactNode;
+   children: ReactNode;
 }
 
 interface DarkModeContextData {
-  darkMode: boolean;
-  handleSetDarkMode: () => void;
+   darkMode: boolean;
+   handleSetDarkMode: () => void;
 }
 
 const DarkModeContext = createContext<DarkModeContextData>(
-  {} as DarkModeContextData
+   {} as DarkModeContextData
 );
 
 export function DarkModeProvider({ children }: DarkModeProviderProps) {
-  const [darkMode, setDarkMode] = useState(false);
+   const [darkMode, setDarkMode] = useState(true);
 
-  useEffect(() => {
-    const darkModeSaved = JSON.parse(
-      localStorage.getItem('darkMode') as string
-    );
+   useEffect(() => {
+      const darkModeSaved = JSON.parse(
+         localStorage.getItem("darkMode") as string
+      );
 
-    if (darkModeSaved) {
-      setDarkMode(darkModeSaved);
-    }
-  }, []);
+      if (darkModeSaved) {
+         setDarkMode(darkModeSaved);
+      }
+   }, []);
 
-  function handleSetDarkMode() {
-    // Saving data to local stoge
-    localStorage.setItem(
-      'darkMode',
-      JSON.stringify(!darkMode)
-    );
+   function handleSetDarkMode() {
+      // Saving data to local stoge
+      localStorage.setItem("darkMode", JSON.stringify(!darkMode));
 
-    setDarkMode(!darkMode);
-  }
+      setDarkMode(!darkMode);
+   }
 
-  return (
-    <DarkModeContext.Provider value={{ darkMode, handleSetDarkMode }}>
-      {children}
-    </DarkModeContext.Provider>
-  )
+   return (
+      <DarkModeContext.Provider value={{ darkMode, handleSetDarkMode }}>
+         {children}
+      </DarkModeContext.Provider>
+   );
 }
 
 export function useDarkMode() {
-  const context = useContext(DarkModeContext);
+   const context = useContext(DarkModeContext);
 
-  return context;
+   return context;
 }
