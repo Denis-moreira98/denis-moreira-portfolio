aaa4d85aa0635185bcc2b9aaca31e4c3a83a8e36
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2023-12-12 13:16:29 -0300

    style: novo icone para darkmod e lightmode

--- src/components/ToggleDarkMode/index.tsx
+++ src/components/ToggleDarkMode/index.tsx
@@ -1,20 +1,27 @@
-import { Moon, Sun } from "phosphor-react";
 import { ToggleDarkModeContainer } from "./styles";
+import { FiSun } from "react-icons/fi";
+import { LuMoonStar } from "react-icons/lu";
 
 interface ToggleDarkModeProps {
-  darkMode: boolean;
-  onHandleDarkMode: () => void;
-  width: 'small' | 'large';
+   darkMode: boolean;
+   onHandleDarkMode: () => void;
+   width: "small" | "large";
 }
 
-export function ToggleDarkMode({ width, darkMode, onHandleDarkMode }: ToggleDarkModeProps) {
-  return (
-    <ToggleDarkModeContainer width={width} className="toggle-switch">
-      <label>
-        <input type="checkbox" onClick={onHandleDarkMode}/>
-        <Sun className={`sun-icon ${!darkMode && 'light-selected'}`} />
-        <Moon className={`moon-icon ${darkMode && 'dark-selected'}`} />       
-      </label>
-    </ToggleDarkModeContainer>
-  )
+export function ToggleDarkMode({
+   width,
+   darkMode,
+   onHandleDarkMode,
+}: ToggleDarkModeProps) {
+   return (
+      <ToggleDarkModeContainer width={width} className="toggle-switch">
+         <label>
+            <input type="checkbox" onClick={onHandleDarkMode} />
+            <FiSun className={`sun-icon ${!darkMode && "light-selected"}`} />
+            <LuMoonStar
+               className={`moon-icon ${darkMode && "dark-selected"}`}
+            />
+         </label>
+      </ToggleDarkModeContainer>
+   );
 }
--- src/components/ToggleDarkMode/styles.ts
+++ src/components/ToggleDarkMode/styles.ts
@@ -1,52 +1,53 @@
 import { HtmlHTMLAttributes } from "react";
 import styled from "styled-components";
 
-interface ToggleDarkModeContainerProps extends HtmlHTMLAttributes<HTMLDivElement> {
-  width: 'small' | 'large';
+interface ToggleDarkModeContainerProps
+   extends HtmlHTMLAttributes<HTMLDivElement> {
+   width: "small" | "large";
 }
 
 export const ToggleDarkModeContainer = styled.div<ToggleDarkModeContainerProps>`
-  label {
-    position: absolute;
-    width: ${({width}) => width === 'small' ? '1.5rem' : '1rem'};
-    height: ${({width}) => width === 'small' ? '1.5rem' : '1rem'};
-    background-color: ${({theme}) => theme.colors.text};
-    border-radius: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    overflow: hidden ;
-  }
-
-  input {
-    display: none;
-  }
-
-  svg {
-    &.sun-icon {
+   label {
       position: absolute;
-      left: 4%;
-      font-size: ${({width}) => width === 'small' ? '1rem' : '1.5rem'};
-      color: ${({theme}) => theme.colors.textContrast};
-      margin: 0 0.2rem;
-      transition: .3s;
-    }
+      width: ${({ width }) => (width === "small" ? "1.5rem" : "1rem")};
+      height: ${({ width }) => (width === "small" ? "1.5rem" : "1rem")};
+      background-color: ${({ theme }) => theme.colors.text};
+      border-radius: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      overflow: hidden;
+   }
 
-    &.moon-icon {
-      position: absolute;
-      right: 15%;
-      font-size: ${({width}) => width === 'small' ? '1rem' : '1.5rem'};
-      color: ${({theme}) => theme.colors.textContrast};
-      transition: .3s;
-    }
-  }
+   input {
+      display: none;
+   }
 
-  .sun-icon.light-selected {
-    transform: translateX(2.2rem);
-  }
+   svg {
+      &.sun-icon {
+         position: absolute;
+         left: 4%;
+         font-size: ${({ width }) => (width === "small" ? "1rem" : "1.5rem")};
+         color: ${({ theme }) => theme.colors.textContrast};
+         margin: 0 0.2rem;
+         transition: 0.3s;
+      }
 
-  .moon-icon.dark-selected {
-    transform: translateX(-2.2rem);
-  }
+      &.moon-icon {
+         position: absolute;
+         right: 15%;
+         font-size: ${({ width }) => (width === "small" ? "1rem" : "1.5rem")};
+         color: ${({ theme }) => theme.colors.textContrast};
+         transition: 0.3s;
+      }
+   }
+
+   .sun-icon.light-selected {
+      transform: translateX(2.2rem);
+   }
+
+   .moon-icon.dark-selected {
+      transform: translateX(-2.2rem);
+   }
 `;
